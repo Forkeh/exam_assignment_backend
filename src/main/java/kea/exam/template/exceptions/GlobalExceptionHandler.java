381c3df60aa9d981dcf75fac05cb410554d63b15
Forkeh <forkeh@gmail.com>
2024-06-20 09:41:15 +0200

    init

---
+++ src/main/java/kea/exam/template/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package kea.exam.template.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorObject> handleEntityNotFoundException(EntityNotFoundException exception) {
+
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorObject.setMessage(exception.getMessage());
+        errorObject.setTimestamp(LocalDate.now());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(errorObject);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getAllErrors()
+                .forEach((error) -> {
+                    String key = ((FieldError) error).getField();
+                    String value = error.getDefaultMessage();
+                    errors.put(key, value);
+                });
+
+        return ResponseEntity.badRequest()
+                .body(errors);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorObject> handleEntityNotFoundException(BadRequestException exception) {
+
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage(exception.getMessage());
+        errorObject.setTimestamp(LocalDate.now());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorObject);
+    }
+}
